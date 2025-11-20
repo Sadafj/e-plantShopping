6ce1ddeedd10f9ee23dabdf63d8716c8cd323a50
Sadaf Jabeen <sadafj081@gmail.com>
2025-11-20 23:51:09 +0500

    Update landing page CSS and improve background

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -90,7 +90,7 @@ const handleCheckoutShopping = (e) => {
       </div>
       <div style={{ marginTop: '20px', color: 'black' }} className='total_cart_amount'></div>
       <div className="continue_shopping_btn">
-        <button className="get-started-button" onClick={(e) => handleContinueShopping(e)}>Continue Shopping</button>
+        <button className="continue-Shopping-button" onClick={(e) => handleContinueShopping(e)}>Continue Shopping</button>
         <br />
         <button className="get-started-button1" onClick={(e) => handleCheckoutShopping(e)}>Checkout</button>
       </div>
